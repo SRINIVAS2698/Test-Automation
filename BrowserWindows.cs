219e5f848837326b2834110715a369566832045e
SRINIVAS2698 <52941381+SRINIVAS2698@users.noreply.github.com>
2019-07-16 12:44:19 -0500

    Add files via upload

---
+++ BrowserWindows.cs
@@ -0,0 +1,38 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ViewerTestFramework.Utilities
+{
+    public class BrowserWindows
+    {
+        IWebDriver driver;
+        public BrowserWindows(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+
+        public List<string> GetCurrentWindows()
+        {
+            return driver.WindowHandles.ToList();
+        }
+
+        public void SwitchToWindow(string window)
+        {
+            driver.SwitchTo().Window(window);
+        }
+
+        public void CloseSwitchedToWindow(string window)
+        {
+            driver.SwitchTo().Window(window).Close();
+        }
+
+        public void SwitchToLastWindow()
+        {
+            driver.SwitchTo().Window(driver.WindowHandles.Last());
+        }
+    }
+}
