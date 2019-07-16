9993453983f859cb68c5e0dbb7e58f72c0c39164
SRINIVAS2698 <52941381+SRINIVAS2698@users.noreply.github.com>
2019-07-16 14:33:08 -0500

    Add files via upload

---
+++ HTMLReporting.cs
@@ -0,0 +1,46 @@
+﻿using AventStack.ExtentReports;
+using AventStack.ExtentReports.Reporter;
+using AventStack.ExtentReports.Reporter.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ViewerTestFramework.Utilities
+{
+    public class HTMLReporting
+    {
+        public static ExtentReports extent;
+        public static ExtentTest featureName;
+        public static ExtentTest scenario;
+
+        public static ExtentHtmlReporter HTMLReporter()
+        {
+            var reportPath = Operations.GetFolderPath("HTMLReports") + @"\ViewerTestReport.html";
+            return new ExtentHtmlReporter(reportPath);
+        }
+
+        public static void InitReports()
+        {
+            var htmlReporter = HTMLReporter();
+            htmlReporter.Configuration().ChartVisibilityOnOpen = true;
+            // report title
+            htmlReporter.Configuration().DocumentTitle = "aventstack - ExtentReports";
+            // encoding, default = UTF-8
+            htmlReporter.Configuration().Encoding = "UTF-8";
+            // protocol (http, https)
+            htmlReporter.Configuration().Protocol = Protocol.HTTPS;
+            htmlReporter.Configuration().Theme = AventStack.ExtentReports.Reporter.Configuration.Theme.Dark;
+            htmlReporter.AppendExisting = true;
+            // chart location - top, bottom
+            htmlReporter.Configuration().ChartLocation = ChartLocation.Bottom;
+            // add custom css
+            htmlReporter.Configuration().CSS = "css-string";
+            // add custom javascript
+            htmlReporter.Configuration().JS = "js-string";
+            extent = new ExtentReports();
+            extent.AttachReporter(htmlReporter);
+        }
+    }
+}
---
+++ ImageUtils.cs
@@ -0,0 +1,62 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ViewerTestFramework.Utilities
+{
+    public static class ImageUtils
+    {
+        /// <summary>
+        /// This method will take a screenshot and save it to a local folder
+        /// </summary>
+        /// <returns></returns>
+        public static string TakeScreenShot()
+        {
+            var imgName = DateTime.Now.Ticks.ToString();
+            ITakesScreenshot ts = (ITakesScreenshot)Hooks.driver;
+            Screenshot scr = ts.GetScreenshot();
+            var screenshotPath = Operations.GetFolderPath("Screenshots")+@"\"+imgName+".png";
+            scr.SaveAsFile(screenshotPath, ScreenshotImageFormat.Png);
+            return screenshotPath;
+        }
+
+        /// <summary>
+        /// This method will generate the base64 format of an image
+        /// </summary>
+        /// <param name="imagePath"></param>
+        /// <returns></returns>
+        public static string ConvertToBase64Image(string imagePath)
+        {
+            string imgFormat;
+            using (System.Drawing.Image image = System.Drawing.Image.FromFile(imagePath))
+            {
+                using (MemoryStream m = new MemoryStream())
+                {
+                    string base64String;
+                    image.Save(m, image.RawFormat);
+                    byte[] imageBytes = m.ToArray();
+                    base64String = Convert.ToBase64String(imageBytes);
+                    imgFormat = "data:image/png;base64," + base64String;
+                }
+            }
+            return imgFormat;
+        }
+
+        /// <summary>
+        /// This method will genereate the HTML string for the base64 Image format
+        /// </summary>
+        /// <param name="imagePath"></param>
+        /// <returns></returns>
+        public static string GenerateHTMLForBase64(string imagePath)
+        {
+            var title = ConvertToBase64Image(imagePath);
+            var base64HTMLContent = $@"<img src=""{title}""</img>";  //Note the order of the $@
+            return base64HTMLContent;
+        }
+
+    }
+}
